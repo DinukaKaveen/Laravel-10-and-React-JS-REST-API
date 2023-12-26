bcf88d7e9c6c86abcc5a94814c7f8f49e834d692
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-26 20:12:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import CreateProduct from './pages/CreateProduct';
+import Products from './pages/Products';
+import EditProduct from './pages/EditProduct';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product/create" element={<CreateProduct />} />
+          <Route path="/product/edit/:id" element={<EditProduct />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
