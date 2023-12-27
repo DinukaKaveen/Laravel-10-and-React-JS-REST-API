61814ce8951140edc56e24c419e1583075088e73
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-27 11:57:46 +0530

    create navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,15 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import CreateProduct from './pages/CreateProduct';
 import Products from './pages/Products';
 import EditProduct from './pages/EditProduct';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <NavBar />
         <Routes>
           <Route path="/" element={<Products />} />
-          <Route path="/products" element={<Products />} />
           <Route path="/product/create" element={<CreateProduct />} />
           <Route path="/product/edit/:id" element={<EditProduct />} />
         </Routes>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div>
-        
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-center">
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <li>
+                <Link
+                  to={"/"}
+                  className="text-gray-900 dark:text-white hover:underline"
+                  aria-current="page"
+                >
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/product/create"}
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Create Product
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <br></br>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
