75dffc8e9cc6425c29068da3f409d029628ec057
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-28 08:18:46 +0530

    create edit product

--- frontend/src/pages/EditProduct.js
+++ frontend/src/pages/EditProduct.js
@@ -1,11 +1,110 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function EditProduct() {
+  const { id } = useParams();
+
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [qty, setQty] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    loadProduct(id);
+  }, []);
+
+  const loadProduct = async (id) => {
+    const result = await axios.get(`http://localhost:8000/api/product/${id}`);
+    setName(result.data.product.name);
+    setQty(result.data.product.qty);
+    setPrice(result.data.product.price);
+  };
+
+  const Submit = async (e) => {
+    e.preventDefault();
+    const product = { name, qty, price };
+
+    await axios
+      .put(`http://localhost:8000/api/product/update/${id}`, product)
+      .then((res) => {
+        if (res.data.success) {
+          setMessage(res.data.message);
+        } else {
+          setMessage(res.data.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      Edit Product
+      <form className="max-w-sm mx-auto" onSubmit={(e) => Submit(e)}>
+
+        {message && <p>{message}</p>}<br/>
+
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="qty"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Qty
+          </label>
+          <input
+            type="number"
+            id="qty"
+            name="qty"
+            onChange={(e) => setQty(e.target.value)}
+            value={qty}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="price"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Price
+          </label>
+          <input
+            type="text"
+            id="price"
+            name="price"
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default EditProduct
+export default EditProduct;
