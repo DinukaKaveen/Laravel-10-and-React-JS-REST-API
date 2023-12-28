061543cfcd5f5eb34d2d615edfb8f9b797a747b6
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-28 09:05:00 +0530

    Update Products.js

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -21,7 +21,7 @@ function Products() {
 
   return (
     <div>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg" style={{margin: 50}}>
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
